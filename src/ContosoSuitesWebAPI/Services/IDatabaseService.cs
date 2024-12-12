6bf39c57e95fda2db5f60609b2ef435c5711e4c8
Michael McKechney <michael@mckechney.com>
2024-12-12 16:26:16 -0500

    Exercise 2.2 step 9

--- src/ContosoSuitesWebAPI/Services/IDatabaseService.cs
+++ src/ContosoSuitesWebAPI/Services/IDatabaseService.cs
@@ -7,4 +7,9 @@ public interface IDatabaseService
     Task<IEnumerable<Hotel>> GetHotels();
     Task<IEnumerable<Booking>> GetBookingsForHotel(int hotelId);
     Task<IEnumerable<Booking>> GetBookingsByHotelAndMinimumDate(int hotelId, DateTime dt);
+
+    Task<IEnumerable<Booking>> GetBookingsMissingHotelRooms();
+
+     Task<IEnumerable<Booking>> GetBookingsWithMultipleHotelRooms();
+
 }
